1a668b031aa612203327c96d2e2441d80ad2c028
zerlingx <centralnode@zerlingx.com>
2021-06-28 20:08:12 +0800

    correctly read img with cv2

--- tmp/test.py
+++ tmp/test.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from numpy.core.defchararray import center
+import cv2
 
-a = np.load("data/center/center.npy")
-print(a.shape)
-print(a)
+img = cv2.imread("data/img/train_img_1.jpg")
+print(img.shape,img.size,img.dtype)
+# cv2.imshow('src',img)
+# cv2.waitKey()
+print(img)
